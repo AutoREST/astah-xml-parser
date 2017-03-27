cb753b387a8c217e49e4539f2420a88dda8a6fed
Marcelo Laser <marcelo.laser@gmail.com>
2017-03-27 14:25:53 -0300

    Minor alterations to the DS, further study necessary.

--- src/main/java/autorest/parser/Data_Structures/UML/UmlAssociation.java
+++ src/main/java/autorest/parser/Data_Structures/UML/UmlAssociation.java
@@ -3,6 +3,7 @@ package autorest.astahxmlparser.umldatastructure;
 import java.util.List;
 import java.util.ArrayList;
 import java.lang.UnsupportedOperationException;
+import autorest.Preprocessor;
 
 public class UmlAssociation extends UmlElement
 {
@@ -17,68 +18,180 @@ public class UmlAssociation extends UmlElement
 	// CONSTRUCTOR
 	public UmlAssociation()
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 
 	// GET METHODS
 	public UmlElement getEnd1()
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlElement getEnd2()
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public List<UmlElement> getEnds()
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlElement getEndById(String id)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlElement getEndByName(String name)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlMultiplicity getMultiplicityEnd1()
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlMultiplicity getMultiplicityEnd2()
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public List<UmlMultiplicity> getMulticities()
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlMultiplicity getMultiplicityByEndId(String id)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlMultiplicity getMultiplicityByEndName(String id)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlAggregation getAggregationEnd1()
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlAggregation getAggregationEnd2()
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlAggregation getAggregationByEndId(String id)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlAggregation getAggregationByEndName(String name)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public List<UmlAggregation> getAggregations()
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 }
--- src/main/java/autorest/parser/Data_Structures/UML/UmlModel.java
+++ src/main/java/autorest/parser/Data_Structures/UML/UmlModel.java
@@ -22,11 +22,15 @@ public class UmlModel
 		associativeClasses = new ArrayList<UmlAssociativeClass>();
 		stereotypes = new ArrayList<UmlStereotype>();
 	}
+	public UmlModel(UmlModel model)
+	{
+		throw new UnsupportedOperationException("TODO");
+	}
 
 	// INTERNAL ACCESS GET METHODS
 	public ArrayList<UmlClass> getClassList()
 	{
-		if(!Preprocessor.FULLACCESS)
+		if(Preprocessor.FULLACCESS)
 		{
 			throw new UnsupportedOperationException("TODO");
 		}
@@ -37,101 +41,269 @@ public class UmlModel
 	}
 	public ArrayList<UmlAssociation> getAssociationList()
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public ArrayList<UmlAssociativeClass> getAssociativeClassList()
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public ArrayList<UmlStereotype> getStereotype()
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlClass getClassByName(String name)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlClass getClassById(String id)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlAssociation getAssociationById(String id)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlAssociation getAssociationByEnd(UmlElement end)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlAssociation getAssociationByEnd(String id)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlAssociativeClass getAssociativeClassByName(String name)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlAssociativeClass getAssociativeClassById(String id)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlAssociativeClass getAssociativeClassByEnd(UmlElement end)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlAssociativeClass getAssociativeClassByEnd(String id)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlStereotype getStereotypeByName(String name)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public UmlStereotype getStereotypeById(String id)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 
 	// ADD METHODS
 	public boolean addClass(UmlClass classy)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public boolean addClass(String id, String name, UmlAccessibility accessibility)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public boolean addAssociation(UmlAssociation association)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public boolean addAssociation(String id, UmlElement end1, UmlElement end2)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public boolean addAssociation(String id, String end1Id, String end2Id)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public boolean addAssociativeClass(UmlAssociativeClass associativeClass)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public boolean addAssociativeClass(String id, String name, UmlAccessibility accessibility, UmlElement end1, UmlElement end2)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public boolean addAssociativeClass(String id, String name, UmlAccessibility accessibility, String end1Id, String end2Id)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public boolean addStereotype(UmlStereotype stereotype)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 	public boolean addStereotype(String id, String name)
 	{
-		throw new UnsupportedOperationException("TODO");
+		if(Preprocessor.FULLACCESS)
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
+		else
+		{
+			throw new UnsupportedOperationException("TODO");
+		}
 	}
 
 	// REMOVE METHODS
