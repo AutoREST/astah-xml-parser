bebfefcfb6f4399d043b43c1e5753e2c748ed13f
Marcelo Laser <marcelo.laser@gmail.com>
2017-03-28 13:32:15 -0300

    Beginning of documentation, work mostly done on UmlAssociation

---
+++ src/main/java/autorest/parser/Data_Structures/UML/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Package containing definitions for the UML Class Diagram data structure.
+ * <p>
+ * Contains the necessary elements for the representation of an UML Class Diagram. This package was developed primarily for use with the AutoREST instance developed by Dumer and Laser.
+ * <p>
+ * This package is still undergoing development.
+ *
+ * @author    Marcelo Schmitt Laser
+ * @version   0.1.2a
+ * @see       autorest
+ * @see       autorest.astahxmlparser
+ */
+package autorest.astahxmlparser.umldatastructure
